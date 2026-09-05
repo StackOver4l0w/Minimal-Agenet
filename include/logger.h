30a5bb2d30ea572498bddd9a3c623b9922f61c26
CelesteNostdlib <rnostdlib@gmail.com>
2026-09-04 17:36:49 -0700

    fix: update linker script usage in build and release workflows

--- include/logger.h
+++ include/logger.h
@@ -6,6 +6,10 @@ INT32 PRINT_FORMATTED_STRING(PCHAR format, ...);
 #define LOGGING_ENABLED
 #endif
 
+#ifndef LOGGING_ENABLED
+#define LOGGING_ENABLED
+#endif
+
 #ifdef LOGGING_ENABLED
 #define LOG_INFO(format, ...) PRINT_FORMATTED_STRING("[INF] " format "\n", ##__VA_ARGS__)
 #define LOG_ERROR(format, ...) PRINT_FORMATTED_STRING("[ERR] " format "\n", ##__VA_ARGS__)
